75ee3939a2c1ccf7a7ff9ad758c0fea3d8986971
Dustin <dustinsinkey1@gmail.com>
2016-02-14 19:56:47 -0800

    Added immutable tree test

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -20,7 +20,7 @@ describe('immutability', () => {
 
   describe('A List', () => {
     function addMovie(currentState, movie){
-      return currentState.push(movie);
+      return currentState.update('movies' movies => movies.push(movie));
     }
 
     it('is immutable', () => {
@@ -29,7 +29,30 @@ describe('immutability', () => {
 
       expect(nextState).to.equal(List.of('Trainspotting', '28 Days Later', 'Sunshine'));
       expect(state).to.equal(List.of('Trainspotting', '28 Days Later'));
-      expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('A tree', () => {
+    function addMovie(currentState, movie){
+      return currentState.set('movies', currentState.get('movies').push(movie));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      }));
+
+      expect(nextState).to.equal(Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      }));
+
     });
 
   });
