b5a7003d2087e6239c0306593af8762c5392b85b
Dustin <dustinsinkey1@gmail.com>
2016-02-16 19:17:11 -0800

    Edited next function to check for winner

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -6,11 +6,15 @@ export function setEntries(state, entries) {
 };
 
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  const entries = state.get('entries').concat(getWinners(state.get('vote')));
+  if(entries.size === 1){
+    return state.remove('vote').remove('entries').set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 export function vote(state, entry) {
@@ -38,3 +42,4 @@ export function next(state){
     entries: entries.skip(2)
   });
 }
+
