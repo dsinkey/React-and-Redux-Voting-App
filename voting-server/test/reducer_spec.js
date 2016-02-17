9b627f0a79eb21018e1e7526520c16e1b426f97c
Dustin <dustinsinkey1@gmail.com>
2016-02-16 20:41:12 -0800

    Added test for SET_ENTRIES and reducer function for it

---
+++ voting-server/test/reducer_spec.js
@@ -0,0 +1,19 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+  
+  it('handles SET_ENTRIES', () =>{
+    const initalState = Map();
+    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+    const nextState = reducer(initalState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['Trainspotting']
+    }));
+  });
+
+
+});
