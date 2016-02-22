2c43458183ea54b14fba0d1f819272a295617c82
Dustin <dustinsinkey1@gmail.com>
2016-02-21 21:11:20 -0800

    Added App.jsx and react-router to index.jsx

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route} from 'react-router';
+import App from '.components/App';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
+
+const routes = <Route component={App}><Route path="/" component={Voting} /></Route>;
 
 ReactDOM.reder(
   <Voting pair={pair} winner="Trainspotting" />, document.getElementById('app')
