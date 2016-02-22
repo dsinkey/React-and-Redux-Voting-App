5a7e5b0788b77fdbd5c1cf42bc6ab4f9cf356e4a
Dustin <dustinsinkey1@gmail.com>
2016-02-22 15:34:49 -0800

    Added Results compopent to index.jsx

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import {Route} from 'react-router';
 import App from '.components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
-
-const routes = <Route component={App}><Route path="/" component={Voting} /></Route>;
+const routes = <Route component={App}><Route path="/results" component={Results} /></Route>;
 
 ReactDOM.reder(
   <Voting pair={pair} winner="Trainspotting" />, document.getElementById('app')
