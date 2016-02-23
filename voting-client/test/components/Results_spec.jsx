419962638d20ddf9b624473b3458cd29a873944a
Dustin <dustinsinkey1@gmail.com>
2016-02-22 17:20:39 -0800

    Added test to see if Results component renders entries with vote count or zero

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -6,5 +6,20 @@ import {expect} from 'chai';
 const {renderIntoDocument, scryRenderedDOMComponentsWithClass} = React.addons.TestUtils;
 
 describe('Results', () => {
-    
+  it("Renders entries with vote count or zero", () => {
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const tally = Map(['Trainspotting': 5]);
+    const component = renderIntoDocument(
+      <Results pair={pair} tally={tally} />
+    );
+    const entires = scryRenderedDOMComponentsWithClass(component, 'entry');
+    const [train, days] = entires.map(e => e.textContent);
+
+    expect(entires.length).to.equal(2);
+    expect(train).to.contain('Trainspotting');
+    expect(train).to.contain('5');
+    expect(days).to.contain('28 Days Later');
+    expect(days).to.contain('0');
+  });
+
 });
