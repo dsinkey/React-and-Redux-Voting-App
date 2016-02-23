63ced93bda41f9102bdd19be6774fbab728fbfd6
Dustin <dustinsinkey1@gmail.com>
2016-02-22 16:59:06 -0800

    Added Results_spec test for Results component

---
+++ voting-client/test/components/Results_spec.jsx
@@ -0,0 +1,10 @@
+import React from 'react/addons';
+import {List, Map} from 'immutable';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+
+const {renderIntoDocument, scryRenderedDOMComponentsWithClass} = React.addons.TestUtils;
+
+describe('Results', () => {
+    
+});
