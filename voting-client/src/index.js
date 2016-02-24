12cebec68b0d3abdc3152d6a7b6d2eb48309878b
Dustin <dustinsinkey1@gmail.com>
2016-02-23 22:51:40 -0800

    redux and reducer to index.js and created temp store with initial voting data

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from '.components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
-const routes = <Route component={App}><Route path="/results" component={Results} /></Route>;
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
+
+const routes = <Route component={App}>
+                <Route path="/results" component={Results} />
+                <Route path="/" component={Voting} />
+                </Route>;
 
 ReactDOM.reder(
-  <Voting pair={pair} winner="Trainspotting" />, document.getElementById('app')
+  <Router history={hashHistory} />{routes}</Router>, document.getElementById('app')
 );
