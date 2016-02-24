1e3a04a9269d5e9d3584a12424c126571883e2c6
Dustin <dustinsinkey1@gmail.com>
2016-02-23 21:07:20 -0800

    Added test to reducer_spec to check SET_STATE

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -4,5 +4,25 @@ import {expect} from 'chai';
 import reducer from '../src/reducer';
 
 describe('reducer', () => {
-    
+
+  it('handles SET_STATE', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({Trainspotting: 1})
+        })
+      })
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally; {Trainspotting: 1}
+      }
+    }));
+  });
 });
