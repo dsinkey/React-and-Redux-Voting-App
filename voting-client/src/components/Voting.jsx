3aa1f983724d82b4c4461a7f15f67df71cf78bfe
Dustin <dustinsinkey1@gmail.com>
2016-02-24 20:24:48 -0800

    Added mapToProps function, used connect to wire up component, and exported Voting component

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -14,3 +14,14 @@ const Voting = React.createClass({
     </div>;
   }
 });
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  };
+}
+
+connect(mapStateToProps)(Voting);
+
+export default Voting;
