020fb8e5d019744107f1c38f6381a4d4dbe20e72
Dustin <dustinsinkey1@gmail.com>
2016-02-24 20:12:24 -0800

    Imported react-redux into index.js as a Provide component and wrapped Router component in it

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from '.components/App';
 import Voting from './components/Voting';
@@ -24,5 +25,8 @@ const routes = <Route component={App}>
                 </Route>;
 
 ReactDOM.reder(
-  <Router history={hashHistory} />{routes}</Router>, document.getElementById('app')
+  <Provier store={store}>
+    <Router history={hashHistory} />{routes}</Router>
+  </Provider>,
+  document.getElementById('app')
 );
