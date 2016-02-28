b14aa7fbf2e89fccb61450de04a3efd969d02e94
Dustin <dustinsinkey1@gmail.com>
2016-02-27 16:28:09 -0800

    Added round to reducer_spec.js

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -24,6 +24,7 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
+        round: 1,
         pair: ['Trainspotting', '28 Days Later']
       },
       entries: []
@@ -33,6 +34,7 @@ describe('reducer', () => {
   it('handles VOTE', () =>{
     const initalState = fromJS({
       vote: {
+        round: 1,
         pair: ['Trainspotting', '28 Days Later']
       },
       entries: []
@@ -43,6 +45,7 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       vote: {
+        round: 1,
         pair: ['Trainspotting', '28 Days Later'],
         tally: {Trainspotting: 1}
       },
